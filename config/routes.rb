720b508e6d485dd5e233a08cef5e0b8ea6f9eb13
Marta Wojtowicz <marta.wojtowicz@polcode.net>
2016-05-12 20:42:26 +0200

    cart#show added

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
   resource :cart, only: [:show] do
-    put 'add/:movie_id', to: 'carts#add', as: :add_to
-    put 'remove/:movie_id', to: 'carts#remove', as: :remove_from
+    put 'add/:product_id', to: 'carts#add', as: :add_to
+    put 'remove/:product_id', to: 'carts#remove', as: :remove_from
   end
 
   devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
